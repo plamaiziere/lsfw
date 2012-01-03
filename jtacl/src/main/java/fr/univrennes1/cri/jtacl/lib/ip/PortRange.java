4b7db7aba8f6c6724b27b8ad46e0b06237098d90
Patrick Lamaiziere <patrick.dev@davenulle.org>
2012-01-03 18:27:14 +0000

    - allow make range with first port > last port - javadoc

--- jtacl/src/main/java/fr/univrennes1/cri/jtacl/lib/ip/PortRange.java
+++ jtacl/src/main/java/fr/univrennes1/cri/jtacl/lib/ip/PortRange.java
@@ -17,26 +17,59 @@ package fr.univrennes1.cri.jtacl.lib.ip;
  * A range of ports [0..65535]
  * @author Patrick Lamaiziere <patrick.lamaiziere@univ-rennes1.fr>
  */
-public class PortRange {
+ public class PortRange {
 	private int _firstPort = -1;
 	private int _lastPort = -1;
 
 	public static final int MAX = 65535;
 
+	/**
+	 * Construct a new range with only one port [port..port].
+	 * @param port port of the range.
+	 */
 	public PortRange(int port) {
+		if (port < 0 || port > MAX)
+			throw new IllegalArgumentException("Invalid port: " + port);
 		_firstPort = port;
 		_lastPort = port;
 	}
 
+	/**
+	 * Construct a new range between two ports. lastPort may be &lt firstPort.
+	 * @param firstPort first port of the range.
+	 * @param lastPort last port of the range.
+	 */
 	public PortRange(int firstPort, int lastPort) {
-		_firstPort = firstPort;
-		_lastPort = lastPort;
+		if (firstPort < 0 || firstPort > MAX || lastPort < 0 || lastPort > MAX)
+			throw new IllegalArgumentException("Invalid port range first: " +
+					firstPort + " last: " + lastPort);
+
+		/*
+		 * check order
+		 */
+		if (firstPort <= lastPort) {
+			_firstPort = firstPort;
+			_lastPort = lastPort;
+		} else {
+			_lastPort = firstPort;
+			_firstPort = lastPort;
+		}
 	}
 
+	/**
+	 * Returns the first port of the range.
+	 * Ensures that firstPort() <= lastPort().
+	 * @return the first port of the range..
+	 */
 	public int getFirstPort() {
 		return _firstPort;
 	}
 
+	/**
+	 * Returns the last port of the range.
+	 * Ensures that lastPort() >= firstPort().
+	 * @return the last port of the range..
+	 */
 	public int getLastPort() {
 		return _lastPort;
 	}
