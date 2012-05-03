271e1fbf9acad3359970f2beaf2a82a3281be68a
Patrick Lamaiziere <patrick.dev@davenulle.org>
2012-05-03 11:51:45 +0000

    add override annotations

--- jtacl/src/main/java/fr/univrennes1/cri/jtacl/shell/DevNull.java
+++ jtacl/src/main/java/fr/univrennes1/cri/jtacl/shell/DevNull.java
@@ -26,10 +26,19 @@ public final class DevNull {
 	 */
     public final static PrintStream out
 			= new PrintStream(new OutputStream() {
+		@Override
         public void close() {}
+		
+		@Override
         public void flush() {}
+		
+		@Override
         public void write(byte[] b) {}
+		
+		@Override
         public void write(byte[] b, int off, int len) {}
+		
+		@Override
         public void write(int b) {}
     } );
 }
