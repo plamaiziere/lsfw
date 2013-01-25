cc6c207cb6e00f59bfe737ca81fefc1a4793e316
Patrick Lamaiziere <patrick.dev@davenulle.org>
2013-01-25 07:49:44 +0000

    add allow broadcast flag

--- jtacl/src/main/java/fr/univrennes1/cri/jtacl/equipments/checkpoint/CpNetworkIP.java
+++ jtacl/src/main/java/fr/univrennes1/cri/jtacl/equipments/checkpoint/CpNetworkIP.java
@@ -25,26 +25,36 @@ public class CpNetworkIP extends CpNetworkObject {
 	/* ip/network address */
 	protected IPNet _ip;
 
+	protected boolean _allowBroadcast;
+
 	/**
 	 * Construct a new checkpoint IP network object
-	 * @param name service name
+	 * @param name object name name
 	 * @param className checkpoint class name
 	 * @param comment comment
 	 * @param ip ip/network address
+	 * @param allowBroadcast true if the broadcast address is allowed.
 	 */
-	public CpNetworkIP(String name,	String className, String comment, IPNet ip) {
+	public CpNetworkIP(String name,	String className, String comment, IPNet ip,
+			boolean allowBroadcast) {
 
 		super(name, className, comment, CpNetworkType.IP);
 		_ip = ip;
+		_allowBroadcast = allowBroadcast;
 	}
 
 	public IPNet getIp() {
 		return _ip;
 	}
 
+	public boolean broadcastAllowed() {
+		return _allowBroadcast;
+	}
+
 	@Override
 	public String toString() {
 		return _name + ", " + _className + ", " + _comment + ", " +  _type
-				+ ", IPNet=" + _ip.toString("i:");
+				+ ", IPNet=" + _ip.toString("i::") + ", allow_broadcast="
+				+ _allowBroadcast;
 	}
 }
