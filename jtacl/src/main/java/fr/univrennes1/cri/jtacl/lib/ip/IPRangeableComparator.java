bb6b277cb25c176bb41dacf58426e8abfdf5271b
Patrick Lamaiziere <patrick.dev@davenulle.org>
2013-02-07 15:21:01 +0000

    Comparator for IPRangeable

---
+++ jtacl/src/main/java/fr/univrennes1/cri/jtacl/lib/ip/IPRangeableComparator.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2013, Universite de Rennes 1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the ESUP-Portail license as published by the
+ * ESUP-Portail consortium.
+ *
+ * Alternatively, this software may be distributed under the terms of BSD
+ * license.
+ *
+ * See COPYING for more details.
+ */
+
+package fr.univrennes1.cri.jtacl.lib.ip;
+
+import java.util.Comparator;
+
+/**
+ * IPRangeable comparator.
+ *
+ * @author Patrick Lamaiziere <patrick.lamaiziere@univ-rennes1.fr>
+ */
+public class IPRangeableComparator implements Comparator<IPRangeable> {
+
+	@Override
+	public int compare(IPRangeable ip1, IPRangeable ip2) {
+		return ip1.getIpFirst().compareTo(ip2.getIpFirst());
+	}
+}
