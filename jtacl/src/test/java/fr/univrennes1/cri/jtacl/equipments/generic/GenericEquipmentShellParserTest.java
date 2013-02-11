dfb8103bc02813330ae757d9103e37800c6a6df8
Patrick Lamaiziere <patrick.dev@davenulle.org>
2013-02-11 13:51:04 +0000

    test case for GenericEquipmentShellParser

--- jtacl/src/test/java/fr/univrennes1/cri/jtacl/equipments/generic/GenericEquipmentShellParserTest.java
+++ jtacl/src/test/java/fr/univrennes1/cri/jtacl/equipments/generic/GenericEquipmentShellParserTest.java
@@ -15,22 +15,29 @@ package fr.univrennes1.cri.jtacl.equipments.generic;
 
 import java.util.List;
 import junit.framework.TestCase;
+import org.parboiled.Parboiled;
+import org.parboiled.parserunners.ReportingParseRunner;
+import org.parboiled.support.ParsingResult;
 
 /**
  *
  * @author Patrick Lamaiziere <patrick.lamaiziere@univ-rennes1.fr>
  */
 public class GenericEquipmentShellParserTest extends TestCase {
-	
+
+	GenericEquipmentShellParser parser =
+		Parboiled.createParser(GenericEquipmentShellParser.class);
+	ParsingResult<?> result;
+
 	public GenericEquipmentShellParserTest(String testName) {
 		super(testName);
 	}
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
-	
+
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
@@ -43,12 +50,42 @@ public class GenericEquipmentShellParserTest extends TestCase {
 		System.out.println("expandFormat");
 		String format = ";%i %o";
 		GenericEquipmentShellParser instance = new GenericEquipmentShellParser();
-		
-		List<String> result = instance.expandFormat(format);
-		assertEquals(";", result.get(0));
-		assertEquals("%i", result.get(1));
-		assertEquals(" ", result.get(2));
-		assertEquals("%o", result.get(3));
+
+		List<String> fmtresult = GenericEquipmentShellParser.expandFormat(format);
+		assertEquals(";", fmtresult.get(0));
+		assertEquals("%i", fmtresult.get(1));
+		assertEquals(" ", fmtresult.get(2));
+		assertEquals("%o", fmtresult.get(3));
+	}
+
+	/**
+	 * Test of xref ip.
+	 */
+	public void testXrefIP() {
+		System.out.println("xref-ip");
+
+		ReportingParseRunner parseRunner =
+				new ReportingParseRunner(parser.CommandXrefIp());
+		String line = "xref    ip     fmt  \"toto\"    host     IP";
+
+		result = parseRunner.run(line);
+		assertTrue(result.matched);
+		assertEquals("xref-ip", parser.getCommand());
+	}
+
+	/**
+	 * Test of xref service.
+	 */
+	public void testXrefService() {
+		System.out.println("xref-service");
+
+		ReportingParseRunner parseRunner =
+				new ReportingParseRunner(parser.CommandXrefService());
+		String line = "xref    service";
+
+		result = parseRunner.run(line);
+		assertTrue(result.matched);
+		assertEquals("xref-service", parser.getCommand());
 	}
 
 }
