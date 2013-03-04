545928a09b391965636e3e11f5da32339cfe3290
Patrick Lamaiziere <patrick.dev@davenulle.org>
2013-03-04 13:25:46 +0000

    Enum for the expected result of a policy (ACCEPT/DENY)

---
+++ jtacl/src/main/java/fr/univrennes1/cri/jtacl/policies/PolicyExpect.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2013, Universite de Rennes 1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the ESUP-Portail license as published by the
+ * ESUP-Portail consortium.
+ *
+ * Alternatively, this software may be distributed under the terms of BSD
+ * license.
+ *
+ * See COPYING for more details.
+ */
+
+package fr.univrennes1.cri.jtacl.policies;
+
+/**
+ * The expected result of a Policy.
+ *
+ * @author Patrick Lamaiziere <patrick.lamaiziere@univ-rennes1.fr>
+ */
+public enum PolicyExpect {
+	ACCEPT,
+	DENY
+}
