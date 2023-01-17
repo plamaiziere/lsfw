0ab34dd7cb5979956b37d438c4704c4250023a19
plamaizi <patrick.lamaiziere@univ-rennes1.fr>
2023-01-17 11:00:36 +0100

    drop ESUP license / keep only BSD license

--- src/main/java/fr/univrennes1/cri/jtacl/shell/ShellParser.java
+++ src/main/java/fr/univrennes1/cri/jtacl/shell/ShellParser.java
@@ -2,11 +2,7 @@
  * Copyright (c) 2010 - 2021, Universite de Rennes 1
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the ESUP-Portail license as published by the
- * ESUP-Portail consortium.
- *
- * Alternatively, this software may be distributed under the terms of BSD
- * license.
+ * it under the terms of the 2-clause BSD license.
  *
  * See COPYING for more details.
  */
